64d58e911357532b7a38fd62566ae0e31568ae35
fedefiorini <federico.fiorini@outlook.it>
2020-08-18 16:47:38 +0200

    Fresh commit. Deleted all broken files. Step by step with new file

--- sysmon.c
+++ sysmon.c
@@ -38,23 +38,13 @@
 
 /* INFO: Adjust constants as required */
 /* No. of sampling loops */
-#define ITERATIONS		100
-/* No. of pages */
-#define PAGES_TOTAL  	1000000
+#define ITERATIONS		50
 /* Sampling interval */
 #define TIME_INTERVAL	15
-/* Ranges of page no. */
-#define VH			200
-#define H				150
-#define M				100
-#define L   		64
-#define VL_MAX	10
-#define VL_MIN	5      // Check these values...
 /**************************************/
 
 /* Variables declaration */
 struct timer_list stimer;
-int page_heat[PAGES_TOTAL];
 
 static int process_id;
 module_param(process_id, int, S_IRUGO|S_IWUSR); //Define process_id as module parameter
@@ -77,7 +67,7 @@ static void timer_handler(struct timer_list *aTimer)
   {
     /* Failed to get some results in scanning pages.
         Something must've gone wrong... */
-    printk("SysMon: Scanning Page Table Failed....\n");
+    printk("Profiling: Scanning Page Table Failed....\n");
   }
 }
 
@@ -88,7 +78,7 @@ static int __init timer_init(void)
   stimer.expires  = jiffies + (TIME_INTERVAL * HZ);
 
   add_timer(&stimer);
-  printk("SysMon: Module Init!\n");
+  printk("Profiling Module Loaded!\n");
 
   return 0;
 }
@@ -96,7 +86,7 @@ static int __init timer_init(void)
 static void __exit timer_exit(void)
 {
   del_timer(&stimer);
-  printk("SysMon: Module Unloaded!\n");
+  printk("Profiling Module Unloaded!\n");
 
   return;
 }
@@ -105,181 +95,115 @@ static void __exit timer_exit(void)
 /* Memory scanning implementation */
 static task_struct *get_process(void)
 {
-  struct pid *pid;
+  struct pid *pid = NULL;
+  struct task_struct *process = NULL;
 
-  /* Check that process_id has been passed correctly */
   if (process_id != 0)
   {
+    /* Find PID structure using the PID number */
     pid = find_vpid(process_id);
+    /* Retrieve the process from its PID */
+    process = get_pid_task(pid, PIDTYPE_PID);
   }
-
-  return pid_task(pid, PIDTYPE_PID);
+  return process;
 }
 
 static int scan_pgtable(void)
 {
-  /* Benchmark (profiled) process */
-  struct task_struct *proc;
+  /* Process profiled */
+  task_struct *process = NULL;
 
-  /* Page Table levels (implemented 5-level) */
-  pgd_t *pgd = NULL;
-  p4d_t *p4d = NULL;
-  pud_t *pud = NULL;
-  pmd_t *pmd = NULL;
-  pte_t *ptep, pte;
+  /* Page Table Walk */
+  /* 5-level implemented (unused levels are folded at runtime) */
+  pgd_t *pgd  = NULL;
+  p4d_t *p4d  = NULL;
+  pud_t *pud  = NULL;
+  pmd_t *pmd  = NULL;
+  pte_t *ptep = NULL;
+  pte_t *pte  = NULL;
 
-  spinlock_t *ptl;
+  spinlock_t *lock;
 
-  /* Memory management variables */
+  /* Process' memory management variables */
   struct mm_struct *mm;
   struct vm_area_struct *vma;
+
+  unsigned long address = 0;
   unsigned long start   = 0;
   unsigned long end     = 0;
-  unsigned long address = 0;
 
-  /* Memory report variables */
-  int num_vpages    = 0;
-  int cycle_index   = 0;  // Loop counter
-  // int hot_pages[ITERATIONS];
+  /* Report variables and counters */
+  int cycle_index   = 0;
   int i             = 0;
-  //
-  // /* Temporary counters */
-  int pg_cnt        = 0;
-  int num_cur_pg    = 0;
-  //
-  // /* Page "heat" variables */
-  // int h             = 0;
-  // int m             = 0;
-  // int l             = 0;
-  // int l2            = 0;
-  // int l3            = 0;
-  // int l4            = 0;
-  //
-  // int all_pages     = 0;  // Total no. of pages
-  // int avg_hotpage   = 0;  // Avg no. of hot pages (per iteration)
-  // int num_access    = 0;  // Total no. of memory accesses (all pages)
-  // int avg_pg_util   = 0;  // Avg utilization for each page
+  long num_vpages   = 0;
 
-  if ((proc = get_process()) == NULL)
+  if ((process = get_process()) == NULL)
   {
-    // Something went wrong...
-    printk("SysMon: Get No Process Handle. Exit and Try Again \n");
+    /* Something went wrong looking for the process */
+    printk("Profiling: Can't retrieve process. Exit and Retry...\n");
     return 0;
   }
   else
   {
-    mm = proc->mm;
+    mm = process->mm;
     if (mm == NULL)
     {
-      // Something went wrong...
-      printk("SysMon: MM is NULL. Exit and Try Again \n");
+      /* Something went wrong accessing process' memory */
+      printk("Profiling: MM is NULL. Exit and Retry...\n");
       return 0;
     }
-
-    for (i = 0; i < PAGES_TOTAL; i++) page_heat[i] = -1;
-    // for (i = 0; i < ITERATIONS; i++) hot_pages[i] = 0;
-
-    for (cycle_index = 0; cycle_index < ITERATIONS; cycle_index++)
+    else
     {
-      // Start scanning each VMA
-      for (vma = mm->mmap; vma; vma = vma->vm_next)
+      /* PROFILING: PAGE WALK AND SAMPLING OF ACCESS_BIT */
+      for (cycle_index = 0; cycle_index < ITERATIONS; cycle_index++)
       {
-        start = vma->vm_start;
-        end   = vma->vm_end;
-        mm    = vma->vm_mm;
-
-        /* Check all pages for each VMA */
-        for (address = start; address < end; address += PAGE_SIZE)
-        {
-          /* Scan page table levels for each page */
-          pgd = pgd_offset(mm, address);
-          if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
-            continue;
-          p4d = p4d_offset(pgd, address);
-          if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d)))
-            continue;
-          pud = pud_offset(p4d, address);
-          if (pud_none(*pud) || unlikely(pud_bad(*pud)))
-            continue;
-          pmd = pmd_offset(pud, address);
-          if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd)))
-            continue;
-
-          ptep = pte_offset_map_lock(mm, pmd, address, &ptl);
-          pte  = *ptep;
-          if (pte_present(pte))
+          /* Scan each vma */
+          for (vma = mm->mmap; vma; vma->vm_next)
           {
-            if (pte_young(pte))
+            start = vma->vm_start;
+            end   = vma->vm_end;
+            mm    = vma->vm_mm;
+
+            /* Scan all pages belonging to the vma */
+            for (address = start; address < end; address += PAGE_SIZE)
             {
-              // Hot Page. Reset and clear Access bit
-              pte = pte_mkold(pte);
-              set_pte_at(mm, address, ptep, pte);
-            }
-          }
-          else
-          {
-            // No Page
-            pte_unmap_unlock(ptep, ptl);
-            continue;
-          }
-          pte_unmap_unlock(ptep, ptl);
-        } /* for (address) */
-      } /* for (vma) */
+              /* Scan Page Tables */
+              pgd = pgd_offset(mm, address);
+              if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd))) continue;
+              p4d = p4d_offset(pgd, address);
+              if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d))) continue;
+              pud = pud_offset(p4d, address);
+              if (pud_none(*pud) || unlikely(pud_bad(*pud))) continue;
+              pmd = pmd_offset(pud, address);
+              if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd))) continue;
 
-      // Count no. of hot pages
-      num_vpages = 0;
-      i = 0;
-      for (vma = mm->mmap; vma; vma = vma->vm_next)
-      {
-        start   = vma->vm_start;
-        end     = vma->vm_end;
-        mm      = vma->vm_mm;
+              ptep  = pte_offset_map_lock(mm, pmd, address, lock);
+              pte   = *ptep;
 
-        pg_cnt = 0;
-        for (address = start; address < end; address += PAGE_SIZE)
-        {
-          /* Scan page table levels for each page */
-          pgd = pgd_offset(mm, address);
-          if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
-            continue;
-          p4d = p4d_offset(pgd, address);
-          if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d)))
-            continue;
-          pud = pud_offset(p4d, address);
-          if (pud_none(*pud) || unlikely(pud_bad(*pud)))
-            continue;
-          pmd = pmd_offset(pud, address);
-          if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd)))
-            continue;
+              if (pte_present(pte))
+              {
+                num_vpages++;   /* Increase page counter */
+                if (pte_young(pte))
+                {
+                  /* Hot Page. Clear access bit */
+                  pte = pte_mkold(pte);
+                  set_pte_at(mm, address, ptep, pte);
+                  pte_unmap_unlock(ptep, lock);
+                }
+              }
+              else
+              {
+                pte_unmap_unlock(pte, lock);
+                continue;
+              }
+            } /* for address */
+          } /* for vma */
+      } /* for iterations */
 
-          ptep = pte_offset_map_lock(mm, pmd, address, &ptl);
-          pte  = *ptep;
-
-          if (pte_present(pte))
-          {
-            if (pte_young(pte))
-            {
-              // Hot Pages
-              num_cur_pg = pg_cnt + num_vpages;
-              page_heat[num_cur_pg]++;
-              // hot_pages[cycle_index]++;
-            }
-          }
-          pg_cnt++;
-
-          pte_unmap_unlock(ptep, ptl);
-        } /* for (address) */
-        i++;
-        num_vpages += (int)((end - start) / PAGE_SIZE);
-        printk("Num_vpages %d vma %d\n", num_vpages, i);
-      } /* for (vma) */
-    } /* for (iterations) */
-    /******************************OUTPUT******************************/
-    /******************************************************************/
-
-    return 1;
-  }
-}
+      printk("Number of virtual pages: %ld\n", num_vpages);
+    } /* if mm */
+  } /* if process */
+} /* end of function */
 /**********************************/
 
 /* Module configuration */
