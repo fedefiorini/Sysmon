6ef24dc808c4fe43ee2834aa5fba753096af1368
fedefiorini <federico.fiorini@outlook.it>
2020-09-01 16:36:24 +0200

    Working version. 10 sampling cycles. Only page hotness

--- sysmon.c
+++ sysmon.c
@@ -38,9 +38,11 @@
 
 /* INFO: Adjust constants as required */
 /* No. of sampling loops */
-#define ITERATIONS		50
+#define ITERATIONS		10
 /* Sampling interval */
-#define TIME_INTERVAL	15
+#define TIME_INTERVAL	20
+/* Total no. of pages (estimate) */
+#define PAGES_TOTAL   100000
 /**************************************/
 
 /* Variables declaration */
@@ -100,13 +102,23 @@ static struct task_struct * get_process(void)
 
   if (process_id != 0)
   {
+    printk("Process ID: %d\n", process_id);
     /* Find PID structure using the PID number */
     pid = find_vpid(process_id);
     /* Retrieve the process from its PID */
     process = get_pid_task(pid, PIDTYPE_PID);
+
+    printk("Process status: %ld\n", process->state);
   }
   return process;
 }
+
+/* Helper function: determines whether the VMA is shared with other processes.
+ * If so, it's skipped (can't swap-out without causing undefined behaviour) */
+// static int is_shared(struct vm_area_struct *vma)
+// {
+//   return 0;
+// }
 
 static int scan_pgtable(void)
 {
@@ -125,7 +137,7 @@ static int scan_pgtable(void)
   spinlock_t *lock;
 
   /* Process' memory management variables */
-  struct mm_struct *mm;
+  struct mm_struct      *mm;
   struct vm_area_struct *vma;
 
   unsigned long address = 0;
@@ -133,9 +145,12 @@ static int scan_pgtable(void)
   unsigned long end     = 0;
 
   /* Report variables and counters */
-  int cycle_index   = 0;
-  int i             = 0;
-  long num_vpages   = 0;
+  int   cycle_index   = 0;
+  int   i             = 0;
+  int   num_vpages    = 0;
+  int   num_hot_pages = 0;
+  int   current_page  = 0;
+  static int   page_heat[PAGES_TOTAL];
 
   if ((process = get_process()) == NULL)
   {
@@ -188,7 +203,6 @@ static int scan_pgtable(void)
                   /* Hot Page. Clear access bit */
                   pte = pte_mkold(pte);
                   set_pte_at(mm, address, ptep, pte);
-                  pte_unmap_unlock(ptep, lock);
                 }
               }
               else
@@ -196,11 +210,71 @@ static int scan_pgtable(void)
                 pte_unmap_unlock(pte, lock);
                 continue;
               }
+              pte_unmap_unlock(pte, lock);
             } /* for address */
           } /* for vma */
       } /* for iterations */
+      printk("Number of virtual pages: %d\n", num_vpages);
 
-      printk("Number of virtual pages: %ld\n", num_vpages);
+      /* Initializing page hotness array */
+      for (i = 0; i < PAGES_TOTAL; i++) page_heat[i] = -1;
+
+      /* PROFILING: PAGE WALK AND REGISTERING PAGE HOTNESS */
+      for (cycle_index = 0; cycle_index < ITERATIONS; cycle_index++)
+      {
+          /* Scan each vma */
+          for (vma = mm->mmap; vma; vma = vma->vm_next)
+          {
+            start = vma->vm_start;
+            end   = vma->vm_end;
+            mm    = vma->vm_mm;
+
+            /* Scan all pages belonging to the vma */
+            for (address = start; address < end; address += PAGE_SIZE)
+            {
+              /* Scan Page Tables */
+              pgd = pgd_offset(mm, address);
+              if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd))) continue;
+              p4d = p4d_offset(pgd, address);
+              if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d))) continue;
+              pud = pud_offset(p4d, address);
+              if (pud_none(*pud) || unlikely(pud_bad(*pud))) continue;
+              pmd = pmd_offset(pud, address);
+              if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd))) continue;
+
+              ptep  = pte_offset_map_lock(mm, pmd, address, &lock);
+              pte   = *ptep;
+
+              if (pte_present(pte))
+              {
+                if (pte_young(pte))
+                {
+                  /* Hot Page. Register result */
+                  page_heat[current_page]++;
+                }
+                current_page++;
+              }
+              else
+              {
+                pte_unmap_unlock(pte, lock);
+                continue;
+              }
+              pte_unmap_unlock(pte, lock);
+            } /* for address */
+          } /* for vma */
+      } /* for iterations */
+      /********************** RESULTS ********************/
+      for (i = 0; i < PAGES_TOTAL; i++)
+      {
+        if (page_heat[i] > -1)
+        {
+          printk("HOT PAGE %d, COUNTER: %d\n", i, page_heat[i]);
+          num_hot_pages++;
+        }
+      }
+
+      printk("No. Pages: %d. Hot Pages: %d\n", num_vpages, num_hot_pages);
+      /****************************************************/
     } /* if mm */
   } /* if process */
 
